bb41de7ffdc77aeb21f03c058e74a8e9cc508acd
Emanuel <emarqu@pm.me>
2024-06-13 22:37:07 -0700

    read inputs/

--- tint.py
+++ tint.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from PIL import Image
+from os import listdir
+from os.path import isfile, join
 
 # Change foreground
 def new_fore(color):
@@ -10,8 +12,18 @@ def new_back(color):
     pass
 
 # Get images size
-def get_image(img_path):
-    im = Image.open(img_path, 'r')
+def get_image(img):
+    img = 'inputs/' + img
+    im = Image.open(img, 'r')
     width, height = im.size
     pixel_values = list(im.getdata())
+    print(width)
+    print(height)
 
+# Get QR codes
+inputs = 'inputs/'
+qr_codes = [f for f in listdir(inputs) if isfile(join(inputs, f))]
+
+# Manipulate QR codes
+for q in qr_codes:
+    get_image(q)

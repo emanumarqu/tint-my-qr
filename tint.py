fa3f14fe5d9e2f6d3e6b982c52b424caeb58b95a
Emanuel <emarqu@pm.me>
2024-06-13 22:04:01 -0700

    Pillow !

--- tint.py
+++ tint.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+from PIL import Image
+
+# Change foreground
+def new_fore(color):
+    pass
+
+# Change background
+def new_back(color):
+    pass
+
+# Get images size
+def get_image(img_path):
+    im = Image.open(img_path, 'r')
+    width, height = im.size
+    pixel_values = list(im.getdata())
 

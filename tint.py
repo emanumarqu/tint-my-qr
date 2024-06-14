7460e8c4ded3c6a935ee6d4ba42b9f4b138e3acc
Emanuel <emarqu@pm.me>
2024-06-14 15:30:37 -0700

    new stack code for PIL

--- tint.py
+++ tint.py
@@ -15,27 +15,25 @@ from os.path import join
 # Change pixels' colors
 def set_fore_back(pixels, color):
     print(str(len(pixels)) + ' pixels will be changed to ' + str(color))
-    for p in pixels:
-        print(p)
 
-# Process image
-def process_qr(q, f, b):
+
+# Create colorized copy of QR code
+def create_color_copy(q, f, b):
     im = Image.open(q, 'r')
-    width, height = im.size
-    pixel_values = list(im.getdata())
+    new_im_pixels = []
+    for pixel in im.getdata():
+        if pixel == (0, 0, 0, 255):
+            new_im_pixels.append(f)
+        else:
+            new_im_pixels.append(b)
     
-    print(len(pixel_values))
-    fore_back = get_fore_back(pixel_values)
-    
-    # if f != '':
-    #     set_fore_back(fore_back[0], f)
-    # if b != '':
-    #     set_fore_back(fore_back[1], b)
+    new_im = Image.new(im.mode, im.size)
+    new_im.putdata(new_im_pixels)
+    new_im.save('outputs/')
 
 # Get QR codes
 input_dir = 'inputs/'
 qr_codes = [input_dir + f for f in listdir(input_dir) if join(input_dir, f).endswith('.png')]
-print(qr_codes)
 
 # Get transformations
 print('Enter colors as RGBA tuples --> (0, 100, 255, 0.5)')
@@ -44,14 +42,14 @@ print('Enter colors as RGBA tuples --> (0, 100, 255, 0.5)')
 # and validates rgba input
 # 
 foreground = (30, 203, 225, 1)
-# background = (225, 52, 30, 0.5)
+background = (225, 52, 30, 0.5)
 # foreground = input('Foreground color: ')
-background = input('Background color: ')
+# background = input('Background color: ')
 print('\n')
 
 # Process QR codes
 for qrcode in qr_codes:
-    process_qr(qrcode, foreground, background)
+    create_color_copy(qrcode, foreground, background)
 
 # Improvements
 # https://pillow.readthedocs.io/en/latest/reference/ImageFilter.html#PIL.ImageFilter.Filter

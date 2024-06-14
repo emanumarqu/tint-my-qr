31c5f92d51f3dc2ff5a7fde1da7333680419f8df
Emanuel <emarqu@pm.me>
2024-06-13 23:33:27 -0700

    process logic

--- tint.py
+++ tint.py
@@ -3,12 +3,11 @@ from PIL import Image
 from os import listdir
 from os.path import isfile, join
 
-# Change foreground
-def new_fore(color):
-    pass
+# Save foreground & background pixels
+def get_fore_back(img):
+    return [34, 69]
 
-# Change background
-def new_back(color):
+def set_fore_back(pixels, color):
     pass
 
 # Process image
@@ -16,15 +15,23 @@ def process_qr(q, f, b):
     im = Image.open(q, 'r')
     width, height = im.size
     pixel_values = list(im.getdata())
+    
+    im_fb = get_fore_back(q)
+    if f != '':
+        set_fore_back(im_fb[0], f)
+    if b != '':
+        set_fore_back(im_fb[1], b)
 
 # Get QR codes
 input_dir = 'inputs/'
 qr_codes = [input_dir + f for f in listdir(input_dir) if isfile(join(input_dir, f))]
 
 # Process QR codes
-print('Enter colors as RGBA tuples like (0, 255, 100, 0.5)')
+print('Enter colors as RGBA tuples --> (0, 100, 255, 0.5)')
 # add code from other repo to convert hex values to rgba
-foreground = (30, 203, 225, 1) # foreground = input('Foreground color: ')
-background = (225, 52, 30, 0.5) # background = input('Background color: ')
+foreground = (30, 203, 225, 1)
+# background = (225, 52, 30, 0.5)
+# foreground = input('Foreground color: ')
+background = input('Background color: ')
 for qrcode in qr_codes:
     process_qr(qrcode, foreground, background)
